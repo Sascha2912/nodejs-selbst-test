a22e66a0960be9dbd672b0b3d2ead34d4724b6f7
Sascha Holzigel <S.Holzigel@gmx.de>
2025-04-11 14:12:55 +0200

    create own webserver with InMemoryStore

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Module importieren
+const { flaschenpost } = require('flaschenpost');
+const { getApi } = require('./lib/getApi');
+const http = require('http');
+const { InMemoryStore } = require('./lib/store/InMemoryStore');
+const { processenv } = require('processenv');
+
+// AIIFE Asychron Immediately Invoke Function Expression
+(async () => {
+
+    // Logger erstellen
+    const logger = flaschenpost.getLogger();
+    // Store einbinden und initialisieren
+    const store = new InMemoryStore();
+    await store.initialize();
+    // Api einbinden
+    const api = getApi({ store });
+    // Server erstellen und Api übergeben
+    const server = http.createServer(api);
+    // Port aus den Umgebungsvariablen auslesen als fallback manuell port 3000 angeben
+    const port = processenv('PORT', 3_000);
+
+
+    // Server auf Port nach verbindungen hören lassen
+    server.listen(port, () => {
+        logger.info('Server startet', { port });
+    });
+})();
